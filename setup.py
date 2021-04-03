9d346ad5c91c337522439fb0d6a8d4fd04d32642
hyunwoongko <gusdnd852@naver.com>
2021-04-03 22:51:11 +0900

    1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='asian-bart',
-    version='1.0.0',
+    version='1.0.1',
     description='Asian language bart models (En, Ja, Ko, Zh, ECJK)',
     long_description=long_description,
     long_description_content_type='text/markdown',
